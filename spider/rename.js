4bffb37a5eeaf2034f6a33bf8fc148460bc500f6
@veta-create <vita.eksuzyan@bk.ru>
2022-11-02 21:09:09 +0300

    add audio

--- spider/rename.js
+++ spider/rename.js
@@ -87,58 +87,34 @@ function generateName(str) {
   return answer.toLowerCase();
 }
 
-let rename = async (birdName) => {
-  //создаёт папку для птички, называет именем птички транслитом
-  fs.mkdirSync(`birdsData/${generateName(birdName)}/audio`, (err) => {
-    if (err) throw err;
-    console.log("Папка успешно создана");
-  });
-};
+// let addAudioDir = async (birdName) => {
+//   //создаёт папку для птички, называет именем птички транслитом
+//   fs.mkdirSync(`birdsData/${generateName(birdName)}/audio`, (err) => {
+//     if (err) throw err;
+//     console.log("Папка успешно создана");
+//   });
+// };
 
-// for (let i = 0; i < file.birds.length; i++) {
-//     file.birds[i].audioPath = `./birdsData/${generateName(
-//       file.birds[i].name
-//     )}/audio/${generateName(file.birds[i].name)}.mp3`;
-//   fs.writeFileSync("./birdsJSON.json", JSON.stringify(file, null, 4));
+// for(let i = 0; i < file.birds.length; i++) {
+//   addAudioDir(generateName(file.birds[i].name))
 // }
+
 // fs.readdir(srcFolder, (err, directories) => {
 //   if (err) throw err;
 //   for (let i = 0; i < directories.length; i++) {
-//     fs.readdir(
-//       `${directories[i]}/${generateName(file.birds[i].name)}`,
-//       (err, files) => {
-//         console.log(files)
-//         // for (let i = 0; i < files.length; i++) {
-//         //   fs.rename(
-//         //     `${srcFolder}/${files[i]}`,
-//         //     `${srcFolder}/${generateName(file.birds[i].name)}-${i + 1}.jpeg`,
-//         //     (err) => {
-//         //       if (err) throw err;
-//         //       console.log("rename completed!");
-//         //     }
-//         //   );
-//         // }
+//     fs.readdir(`${srcFolder}/${directories[i]}/images`, (err, files) => {
+//       if (err) throw err;
+//       for (let j = 0; j < files.length; j++) {
+//         console.log(files[j]);
+//         fs.rename(
+//           `${srcFolder}/${directories[i]}/images/${files[j]}`,
+//           `${srcFolder}/${directories[i]}/images/${directories[i]}-${i+1}.jpeg`,
+//           (err) => {
+//             if (err) throw err;
+//             console.log("rename completed!");
+//           }
+//         );
 //       }
-//     );
+//     });
 //   }
 // });
-
-fs.readdir(srcFolder, (err, directories) => {
-  if (err) throw err;
-  for (let i = 0; i < directories.length; i++) {
-    fs.readdir(`${srcFolder}/${directories[i]}/images`, (err, files) => {
-      if (err) throw err;
-      for (let j = 0; j < files.length; j++) {
-        console.log(files[j]);
-        fs.rename(
-          `${srcFolder}/${directories[i]}/images/${files[j]}`,
-          `${srcFolder}/${directories[i]}/images/${directories[i]}-${i+1}.jpeg`,
-          (err) => {
-            if (err) throw err;
-            console.log("rename completed!");
-          }
-        );
-      }
-    });
-  }
-});
