a21167423869623b9d86188050031afc693aed62
danyabright <danya.bright@yandex.ru>
2022-11-05 09:20:57 -0400

    Added svgr support

--- pages/header/header.tsx
+++ pages/header/header.tsx
@@ -1,9 +1,11 @@
+import Play from '../../assets/images/play.svg';
 import styles from "./header.module.css";
 
 export default function Header() {
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
+        <Play />
         <img src="../../assets/images/logo.png" alt="logo" />
       </div>
       <div className={styles.searchByName}>
